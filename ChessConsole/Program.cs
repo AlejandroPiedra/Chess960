a51e446d64d46c8096b960e6aa2984a094f403c6
Alexjandropiedra <alexrock9657@gmail.com>
2021-08-09 11:10:34 -0600

    Added Original Chess Code

---
+++ ChessConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Chess960
+{
+    public class Program
+    {
+        static ChessGame game;
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            ConsoleGraphics graphics = new ConsoleGraphics();
+            game = new ChessGame();
+
+            do
+            {
+                game.Draw(graphics);
+                graphics.SwapBuffers();
+                game.Update();
+            } while (game.Running);
+
+            Console.Read();
+        }
+    }
+}
